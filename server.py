8a713a949a95115478c910c26293fdd9591b4958
kid-bravo <bravo@kdditest.com>
2026-03-14 00:46:00 +0700

    Update server.py

--- server.py
+++ server.py
@@ -5,9 +5,6 @@ from bot.asgi import app as bot_app
 
 app = FastAPI(title="api-bot-composed")
 
-# include semua router API
-for route in api_app.router.routes:
-    app.router.routes.append(route)
-
-# mount bot
-app.mount("/", bot_app)
+# mount lebih spesifik dulu
+app.mount("/bot", bot_app)
+app.mount("/", api_app)
